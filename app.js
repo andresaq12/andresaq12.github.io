6f0fa916421eb6509a404dd5d240fb30cada0b37
Andres Alvarez <aalvarezqu@gmail.com>
2024-10-30 00:02:21 -0500

    develop chat

---
+++ app.js
@@ -0,0 +1,21 @@
+import { io } from 'https://cdn.socket.io/4.8.0/socket.io.esm.min.js'
+
+const socket = io("http://localhost:1234")
+
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const messages = document.getElementById('messages')
+
+socket.on('chat message', (msg) => {
+  const item = `<li>${msg}</li>`
+  messages.insertAdjacentHTML('beforeend', item)
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  if (input.value) {
+    socket.emit('chat message', input.value)
+    input.value = ''
+  }
+})
